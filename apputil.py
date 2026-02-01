64689c65adc49c57211becaf81aaa3631272fe1a
Chiragk5699 <ckarachi@iu.edu>
2026-02-01 18:21:00 +0000

    Week 2 Exercise

--- apputil.py
+++ apputil.py
@@ -1,13 +1,22 @@
-import numpy as np
-
-
-# update/add code below ...
-
 def ways(n):
     return None
 
-def lowest_score(names, scores):
-    return None
+def ways(n):
+    '''
+    Given an amount n (in cents), return the number of ways
+    to make change using only pennies (1¢) and nickels (5¢).
+    '''
+    
+    count = 0
+    
+    # Number of nickels can range from 0 up to n//5
+    for nickels in range(n // 5 + 1):
+        # Remaining amount will be made with pennies
+        pennies = n - (nickels * 5)
+        if pennies >= 0:
+            count += 1
+    
+    return count
 
-def sort_names(names, scores):
-    return None
+
+
